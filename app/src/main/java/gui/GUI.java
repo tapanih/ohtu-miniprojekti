990fbcbbcc1bf3a04d1854701a993b0304b6a181
Pinja Vierimaa <pinja.vierimaa@helsinki.fi>
2020-11-16 23:16:29 +0200

    Kirjan lisäystä yksinkertaistettu.

--- app/src/main/java/gui/GUI.java
+++ app/src/main/java/gui/GUI.java
@@ -5,85 +5,55 @@ import java.util.Arrays;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import logiikka.Kirja;
 
 public class GUI extends Application {
+    
+    private int ikkunanLeveys = 300;
+    private int ikkunanKorkeus = 300;
 
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Lisää kirja");
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+    public void start(Stage nayttamo) {
         
-        VBox toinenAsettelu = new VBox();
-        Label addingBooksTekstikomponentti = new Label("Syötä uusi kirja ja sen kirjailija muodossa: Kirja, Kirjailija");
-        Button addingBooks = new Button("Lisää!");
-        TextArea teksti = new TextArea("");
-        Label tekstikomponentti1 = new Label("Kirja: ");
-        Label tekstikomponentti2 = new Label("Kirjailija: ");
+        Button lisaaLukuvinkki = new Button();
+        lisaaLukuvinkki.setText("Lisää kirja");
         
-        teksti.textProperty().addListener((muutos, vanhaArvo, uusiArvo) -> {
-            String[] palat = uusiArvo.split(", ");
-            tekstikomponentti1.setText("Kirja: " + palat[0]);
-            tekstikomponentti2.setText("Kirjailija: " + palat[1]);
-        });
+        VBox valikko = new VBox(10);
+        valikko.getChildren().add(lisaaLukuvinkki);
+        valikko.setAlignment(Pos.CENTER);
         
-        toinenAsettelu.getChildren().addAll(addingBooksTekstikomponentti, teksti, tekstikomponentti1, tekstikomponentti2, addingBooks);
-                
-        VBox kolmasAsettelu = new VBox();
-        Button addingBooks2 = new Button("Lisää toinen");
-        Button menu = new Button("Takaisin alkuun");
-        Label LisääToinenkirja = new Label("Alkuun");
-        Label takaisinAlkuun = new Label("Takaisin alkuun");
-        kolmasAsettelu.getChildren().addAll(LisääToinenkirja, addingBooks2, takaisinAlkuun, menu);
-
-        Scene scene = new Scene(root, 300, 250);
-        Scene toinenScene = new Scene(toinenAsettelu);
-        Scene kolmasScene = new Scene(kolmasAsettelu);
+        Scene paavalikko = new Scene(valikko, ikkunanLeveys, ikkunanKorkeus);
         
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                primaryStage.setScene(toinenScene);
-            }
-        });
+        VBox lisaaAsettelu = new VBox(10);
+        lisaaAsettelu.setPadding(new Insets(10,10,10,10));
         
-        addingBooks.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                Kirja kirja = new Kirja(tekstikomponentti1.getText(), tekstikomponentti1.getText());
-                teksti.clear();
-                tekstikomponentti1.setText("Kirja: ");
-                tekstikomponentti2.setText("Kirjailija: ");
-                primaryStage.setScene(kolmasScene);
-            }
-        });
-
-        addingBooks2.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                primaryStage.setScene(toinenScene);
-            }
-        });
+        Label luoUusiLukuvinkki = new Label("Luo uusi lukuvinkki");
+        Button lisaa = new Button("Lisää!");
+        Label otsikko = new Label("Otsikko: ");
+        TextField otsikkoInput = new TextField();
+        Label kirjailija = new Label("Kirjailija: ");
+        TextField kirjailijaInput = new TextField();
         
-        menu.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                primaryStage.setScene(scene);
-            }
-        });
-
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        lisaaAsettelu.getChildren().addAll(luoUusiLukuvinkki, otsikko, otsikkoInput, kirjailija, kirjailijaInput, lisaa);
+        
+        Scene lukuvinkinLisays = new Scene(lisaaAsettelu, ikkunanLeveys, ikkunanKorkeus);
+        
+        lisaaLukuvinkki.setOnAction(e -> nayttamo.setScene(lukuvinkinLisays));
+        
+        lisaa.setOnAction(e -> nayttamo.setScene(paavalikko));
+        
+        nayttamo.setTitle("Lukuvinkkikirjanpito");
+        nayttamo.setScene(paavalikko);
+        nayttamo.show();
     }
 
 }
