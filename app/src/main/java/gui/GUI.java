9d61b2f687b7a2deb9fe07c6df6f528e0317ad6d
Pinja Vierimaa <pinja.vierimaa@helsinki.fi>
2020-11-16 23:29:15 +0200

    Refaktoroitu GUI-luokan koodi.

--- app/src/main/java/gui/GUI.java
+++ app/src/main/java/gui/GUI.java
@@ -1,28 +1,39 @@
 
 package gui;
 
-import java.util.Arrays;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.control.TextField;
-import logiikka.Kirja;
 
 public class GUI extends Application {
     
-    private int ikkunanLeveys = 300;
-    private int ikkunanKorkeus = 300;
+    private int ikkunanLeveys = 500;
+    private int ikkunanKorkeus = 500;
+    
+    private Scene paavalikko;
+    private Scene lukuvinkinLisays;
+    
+    private Stage nayttamo;
 
     @Override
-    public void start(Stage nayttamo) {
+    public void start(Stage stage) {
+        
+        nayttamo = stage;
+        nayttamo.setTitle("Lukuvinkkikirjanpito");
+        paavalikko = paavalikko();
+        lukuvinkinLisays = lukuvinkinLisays();
+        
+        nayttamo.setScene(paavalikko);
+        nayttamo.show();
+    }
+    
+    private Scene paavalikko() {
         
         Button lisaaLukuvinkki = new Button();
         lisaaLukuvinkki.setText("Lisää kirja");
@@ -31,8 +42,12 @@ public class GUI extends Application {
         valikko.getChildren().add(lisaaLukuvinkki);
         valikko.setAlignment(Pos.CENTER);
         
-        Scene paavalikko = new Scene(valikko, ikkunanLeveys, ikkunanKorkeus);
+        lisaaLukuvinkki.setOnAction(e -> nayttamo.setScene(lukuvinkinLisays));
         
+        return new Scene(valikko, ikkunanLeveys, ikkunanKorkeus);
+    }
+    
+    private Scene lukuvinkinLisays() {
         VBox lisaaAsettelu = new VBox(10);
         lisaaAsettelu.setPadding(new Insets(10,10,10,10));
         
@@ -40,20 +55,16 @@ public class GUI extends Application {
         Button lisaa = new Button("Lisää!");
         Label otsikko = new Label("Otsikko: ");
         TextField otsikkoInput = new TextField();
+        otsikkoInput.setMaxWidth(350);
         Label kirjailija = new Label("Kirjailija: ");
         TextField kirjailijaInput = new TextField();
+        kirjailijaInput.setMaxWidth(350);
         
         lisaaAsettelu.getChildren().addAll(luoUusiLukuvinkki, otsikko, otsikkoInput, kirjailija, kirjailijaInput, lisaa);
         
-        Scene lukuvinkinLisays = new Scene(lisaaAsettelu, ikkunanLeveys, ikkunanKorkeus);
-        
-        lisaaLukuvinkki.setOnAction(e -> nayttamo.setScene(lukuvinkinLisays));
-        
         lisaa.setOnAction(e -> nayttamo.setScene(paavalikko));
         
-        nayttamo.setTitle("Lukuvinkkikirjanpito");
-        nayttamo.setScene(paavalikko);
-        nayttamo.show();
+        return new Scene(lisaaAsettelu, ikkunanLeveys, ikkunanKorkeus);
     }
 
 }
