0f5d5b32bf4ba33014ed558c9ded905771a7bc28
Pinja Vierimaa <pinja.vierimaa@helsinki.fi>
2020-11-16 19:37:45 +0200

    Pakkausjako tehty ja Kirja-luokka luotu.

---
+++ app/src/main/java/logiikka/Kirja.java
@@ -0,0 +1,35 @@
+
+package logiikka;
+
+public class Kirja {
+    
+    private String otsikko;
+    private String kirjailija;
+    
+    public Kirja(String otsikko, String kirjailija) {
+        this.otsikko = otsikko;
+        this.kirjailija = kirjailija;
+    }
+
+    public String getOtsikko() {
+        return otsikko;
+    }
+
+    public String getKirjailija() {
+        return kirjailija;
+    }
+
+    public void setOtsikko(String otsikko) {
+        this.otsikko = otsikko;
+    }
+
+    public void setKirjailija(String kirjailija) {
+        this.kirjailija = kirjailija;
+    }
+
+    @Override
+    public String toString() {
+        return otsikko + ", " + kirjailija;
+    }
+    
+}
---
+++ app/src/main/java/logiikka/Main.java
@@ -0,0 +1,16 @@
+package logiikka;
+
+import gui.GUI;
+import javafx.application.Application;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+//        Kirja kirja = new Kirja("Joulupukin joululoma", "Mauri Kunnas");
+//        System.out.println(kirja);
+//        Kirja toinenKirja = new Kirja("Pandakaksoset joulun jäljillä","Kirsti Manninen");
+//        System.out.println(toinenKirja);
+        Application.launch(GUI.class, args);
+    }
+}
