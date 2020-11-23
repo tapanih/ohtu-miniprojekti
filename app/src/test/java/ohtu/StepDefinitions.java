71e81ae3e0b854e0e11a50d8a908202947b3530b
Pinja Vierimaa <pinja.vierimaa@helsinki.fi>
2020-11-23 02:05:11 +0200

    Checkstyle virheet korjattu testeissä.

--- app/src/test/java/ohtu/StepDefinitions.java
+++ app/src/test/java/ohtu/StepDefinitions.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertEquals;
 
 class IsEverythingCorrect {
     static String isEverythingCorrect(String name, String author, Integer pages) {
-        return ("Correct_Name").equals(name) && ("Correct_Author").equals(author) && (pages>0) ? "Yes" : "No";
+        return ("Correct_Name").equals(name) && ("Correct_Author").equals(author) && (pages > 0) ? "Yes" : "No";
     }
 }
 
@@ -19,19 +19,19 @@ public class StepDefinitions {
     private String actualAnswer;
 
     @Given("book has name {string} author {string} and pages {int}")
-    public void book_has_name_author_and_pages(String name, String author, int pages) {
+    public void bookHasNameAuthorAndPages(String name, String author, int pages) {
         this.name = name;
         this.author = author;
         this.pages = pages;
     }
 
     @When("I ask whether they are filled in or not")
-    public void i_ask_whether_they_are_filled_in_or_not() {
-        if (this.name.isEmpty()){
+    public void iAskWhetherTheyAreFilledInOrNot() {
+        if (this.name.isEmpty()) {
             actualAnswer = "not found";
-        } else if (this.author.isEmpty()){
+        } else if (this.author.isEmpty()) {
             actualAnswer = "not found";
-        } else if (this.pages <= 0){
+        } else if (this.pages <= 0) {
             actualAnswer = "No";
         } else {
             actualAnswer = IsEverythingCorrect.isEverythingCorrect(name, author, pages);
@@ -39,7 +39,7 @@ public class StepDefinitions {
     }
 
     @Then("I should be told {string}")
-    public void i_should_be_told(String expectedAnswer) {
+    public void iShouldBeTold(String expectedAnswer) {
         assertEquals(expectedAnswer, actualAnswer);
     }
 }
