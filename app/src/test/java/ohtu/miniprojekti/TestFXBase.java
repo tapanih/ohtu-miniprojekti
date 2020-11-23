ed35756ab3e6ad9cde1fe68e002615a0f728c405
Tapani Honkanen <tapani.honkanen@hotmail.com>
2020-11-23 12:51:44 +0200

    Added TestFX

---
+++ app/src/test/java/ohtu/miniprojekti/TestFXBase.java
@@ -0,0 +1,37 @@
+package ohtu.miniprojekti;
+
+import javafx.scene.Node;
+import javafx.stage.Stage;
+import org.junit.*;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.testfx.api.FxAssert.verifyThat;
+
+import org.testfx.api.FxAssert;
+import org.testfx.framework.junit.ApplicationTest;
+import gui.GUI;
+import org.testfx.matcher.control.LabeledMatchers;
+import org.testfx.util.WaitForAsyncUtils;
+
+public class TestFXBase extends ApplicationTest {
+
+    @Before
+    public void setUpClass() throws Exception {
+        ApplicationTest.launch(GUI.class);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        stage.show();
+    }
+
+    @Test
+    public void thereIsAButtonForAddingBooks() {
+        WaitForAsyncUtils.waitForFxEvents();
+        FxAssert.verifyThat(".button", LabeledMatchers.hasText("Lisää kirja"));
+    }
+
+    public <T extends Node> T find(final String query) {
+        return lookup(query).query();
+    }
+}
